3c35bbc0712808a7e5a838e2a7317b8d1e8ba711
Thomas Berry <thomasdpb@gmail.com>
2019-10-22 17:27:22 +0100

    grande progresso

--- solution.py
+++ solution.py
@@ -3,8 +3,9 @@
 import sys
 sys.path.insert(1, 'aima-python')
 
-
 from search import *
+
+from copy import deepcopy,copy
 
 class ASARProblem(Problem):
 
@@ -18,28 +19,57 @@ class ASARProblem(Problem):
         self.load(open(filename,'r'))
 
 
+    # TODO change this to implement actions as generator
     def actions(self, state):
         """Return the actions that can be executed in the given
         state. The result would typically be a list, but if there are
         many actions, consider yielding them one at a time in an
         iterator, rather than building them all at once."""
-        raise NotImplementedError
+        
+        """ an action is a (plane,leg) touple"""
+    
+        actions = list()
+
+        for pname,p in state['planes'].items():
+            # pname is the name of the current name
+
+            if p['current'] is None:
+                actions = actions + [{'name':pname,'leg':leg} for leg in state['legs'] if self.airports[leg['from']]['open'] + leg['duration'] < self.airports[leg['to']]['close']]
+            else:
+                for leg in state['legs']:
+                    if leg['from'] == p['current'] and p['ready'] + leg['duration'] < self.airports[leg['to']]['close']:
+                        actions.append({'name':pname,'leg':leg})
+        
+        return actions
+
 
     def result(self, state, action):
         """Return the state that results from executing the given
         action in the given state. The action must be one of
         self.actions(state)."""
-        raise NotImplementedError
+
+        # the values are stored in the correct order
+        state = deepcopy(state)
+        planes,legs,profit = state.values()
+        
+        # the action changes one of the planes
+        plane = planes[action['name']]
+    
+        if p['current'] is None:
+            p['initial'] = action['leg']['from']
+            p['ready'] = action['leg']['open']
+
+        plane['legs'].append(action['leg'])
+        plane['current'] = action['leg']['to']
+        plane['ready'] = plane['ready'] + action['leg']['duration'] + plane['rolltime']
+
+        # TODO: make sure this is deep  equals!!!!!!
+        legs.remove(action['leg'])
 
     def goal_test(self, state):
-        """Return True if the state is a goal. The default method compares the
-        state to self.goal or checks for state in self.goal if it is a
-        list, as specified in the constructor. Override this method if
-        checking against a single self.goal is not enough."""
-        if isinstance(self.goal, list):
-            return is_in(state, self.goal)
-        else:
-            return state == self.goal
+        """Return True if the state is a goal. """
+        # if there are no more legs to atribute and all of the aples
+        return (not state['legs']) and all( p['inital']==p['current'] for p in state['planes'].values())
 
     def path_cost(self, c, state1, action, state2):
         """Return the cost of a solution path that arrives at state2 from
@@ -48,19 +78,18 @@ class ASARProblem(Problem):
         state2.  If the path does matter, it will consider c and maybe state1
         and action. The default method costs 1 for every step in the path."""
 
-        """ path will be the negative of the profit """
+        """path will be the negative of the profit"""
 
-        return c + 1
+        return c - action['leg'][state1['planes'][action['name']]['class']]
 
     def heurisitc(self, n):
         """Return the heuristic of node n"""
-        raise NotImplementedError
+        return - sum( min( l[k] for k in self.classes ) for l in n.state['legs'])
 
     def load(self, f):
         """Loads a problem from a (opened) file object f"""
 
         ### Load the data
-
 
         lines = f.read().splitlines()
         A = [ l for l in lines if  l.find("A")==0 ]
@@ -68,35 +97,64 @@ class ASARProblem(Problem):
         L = [ l for l in lines if  l.find("L")==0 ]
         C = [ l for l in lines if  l.find("C")==0 ]
 
-        # rolltimes is a dictionary which contains the rolltimes for each class
-        self.rolltimes = {s.split()[1]: int(s.split()[2]) for s in C }
+        # rolltimes is a dictionary which contains the classes for each class
+        classes = {s.split()[1]: int(s.split()[2]) for s in C }
 
         # legs is a dictionary which conatins 
         def Leg(s):
-            d = {
+            l = {
                 'from': s[1],
                 'to': s[2],
                 'duration': int(s[3]),
             }
             for i in range( 4, len(s), 2 ):
-                d[s[i]] = int(s[i+1])
-            return d
-        self.legs = [ Leg(s.split()) for s in L ]
+                l[s[i]] = int(s[i+1])
+            return l
+        legs = [ Leg(s.split()) for s in L ]
 
-        print(self.rolltimes)
-        print(self.legs)
+        print(classes)
+        print(legs)
+
+        def Plane(s, c):
+            return {
+                'class': s[2],
+                'rolltime':c,
+                'inital': None,
+                'current': None,
+                'ready': 0,
+                'legs': list()
+            }
+
+        to_minutes = lambda x: (x//100)*60 + x%100
+        self.airports = { s.split()[1]: \
+            {'open':to_minutes(int(s.split()[2])),'close':to_minutes(int(s.split()[3]))} for s in A}
+
+        print()
+        print(self.airports)
+
+        initial_state = {
+            'legs': legs,
+            'planes': {p.split()[1]: Plane(p.split(),classes[p.split()[2]]) for p in P }
+        }
+
+        print(initial_state)
 
         ### Now that the data is loaded we must define the initial states
 
-        #self.initial = 
+        self.initial =  initial_state
 
         # defining a goal state will not be necessary because there is more than
         # one goal state
 
+        self.classes = classes
+
 
     def save(self, f , s):
         """saves a solution state s to a (opened) file object f"""
-        raise NotImplementedError
+
+
+        # TODO Remember to convert back to hours and minutes
+
 
 if __name__ == "__main__":
     ASARProblem('./examples/simple1.txt')
